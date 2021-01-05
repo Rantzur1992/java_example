dd38b66a5e38f32b9abdcfaac0736ce12d21eed4
LAPTOP-IB4DPRIG\rantz <ran.tzur@testproject.io>
2021-01-05 14:30:04 +0200

    New Check

--- src/test/java/JavaExample.java
+++ src/test/java/JavaExample.java
@@ -1,16 +1,40 @@
+/*
+ * Copyright (c) 2020 TestProject LTD. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 import io.testproject.sdk.DriverBuilder;
+import io.testproject.sdk.drivers.GenericDriver;
 import io.testproject.sdk.drivers.web.ChromeDriver;
 import io.testproject.sdk.internal.exceptions.AgentConnectException;
 import io.testproject.sdk.internal.exceptions.InvalidTokenException;
 import io.testproject.sdk.internal.exceptions.ObsoleteVersionException;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-
 import java.io.IOException;
 
+
+
+/* Run a simple Web Test with JUNIT 4*/
 public class JavaExample {
-    public static void main(String[] args) throws IOException, AgentConnectException, ObsoleteVersionException, InvalidTokenException {
+
+    @Test
+    public void runTest() throws IOException, AgentConnectException, ObsoleteVersionException, InvalidTokenException {
 
         ChromeDriver driver = new DriverBuilder<ChromeDriver>(new ChromeOptions()).build(ChromeDriver.class);
 
